2263c4bfb9d2c3b27f6148a9506facc47dce77c4
Yasaswini <yasa2005.04@gmail.com>
2025-11-01 11:40:37 +0530

    authentication system

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { register, login, logout } from '../controllers/authControllers.js';
+import { verifyToken } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+router.post('/logout', verifyToken, logout);
+
+export default router;
